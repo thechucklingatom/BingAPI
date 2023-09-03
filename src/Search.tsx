c822ffed093474f1a6511d18d3ab0b170e09eb05
atom <thechucklingatom@gmail.com>
2023-09-03 15:40:43 -0600

    create api call, add simple print to screen for initial verification

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Search() {
+  const [searchText, setSearchText] = useState('');
+  const [searchResponse, setSearchResponse] = useState(null);
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchText(event.target.value);
+  }
+
+  const searchForStuff = () => {
+    fetch(import.meta.env.VITE_API_URL + `v7.0/search?q=${searchText}`, {
+      method: 'get',
+      headers: new Headers({
+        'Ocp-Apim-Subscription-Key': import.meta.env.VITE_API_KEY
+      })
+    })
+      .then(response => response.json())
+      .then(json => setSearchResponse(json))
+      .catch(error => console.error(error));
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{import.meta.env.VITE_API_KEY}</h1>
+      <label>
+        What would you like to search for? <input name="searchInput" onChange={handleSearchChange}/>
+      </label>
+      <button onClick={searchForStuff}>Search</button>
+
+      {searchResponse && JSON.stringify(searchResponse)} 
     </>
   )
 }
 
-export default App
+
+
+export default Search
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import Search from './Search.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Search />
   </React.StrictMode>,
 )
